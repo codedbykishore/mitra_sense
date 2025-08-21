5b80f5a55b78028fc675f3794fa03ec882055a63
Kishore B <kishore3014b@gmail.com>
2025-08-21 14:40:01 +0530

    fix main.py

--- app/main.py
+++ app/main.py
@@ -4,11 +4,11 @@ from app.routes import input, crisis, peer, family, analytics
 app = FastAPI(title="Mental Health MVP", version="0.1")
 
 # Register routers
-app.include_router(input.router, prefix="/api", tags=["Input"])
-app.include_router(crisis.router, prefix="/api", tags=["Crisis"])
-app.include_router(peer.router, prefix="/api", tags=["Peer"])
-app.include_router(family.router, prefix="/api", tags=["Family"])
-app.include_router(analytics.router, prefix="/api", tags=["Analytics"])
+# app.include_router(input.router, prefix="/api", tags=["Input"])
+# app.include_router(crisis.router, prefix="/api", tags=["Crisis"])
+# app.include_router(peer.router, prefix="/api", tags=["Peer"])
+# app.include_router(family.router, prefix="/api", tags=["Family"])
+# app.include_router(analytics.router, prefix="/api", tags=["Analytics"])
 
 
 @app.get("/ping")
