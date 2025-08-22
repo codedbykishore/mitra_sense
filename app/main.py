72e25ef38bd828a14ed78ad55409cbe0511c2c8c
Kishore B <kishore3014b@gmail.com>
2025-08-22 13:35:33 +0530

    init main.py

--- app/main.py
+++ app/main.py
@@ -1,16 +1,8 @@
 from fastapi import FastAPI
-from app.routes import input, crisis, peer, family, analytics
 
-app = FastAPI(title="Mental Health MVP", version="0.1")
-
-# Register routers
-# app.include_router(input.router, prefix="/api", tags=["Input"])
-# app.include_router(crisis.router, prefix="/api", tags=["Crisis"])
-# app.include_router(peer.router, prefix="/api", tags=["Peer"])
-# app.include_router(family.router, prefix="/api", tags=["Family"])
-# app.include_router(analytics.router, prefix="/api", tags=["Analytics"])
+app = FastAPI()
 
 
-@app.get("/ping")
-def ping():
-    return {"msg": "pong"}
+@app.get("/")
+def root():
+    return {"message": "root end point"}
